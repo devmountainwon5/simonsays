eed0b8ac52563b8a2d8046e0f6678d8e5ebd1ac6
Brent Whitehead <brent.whitehead@skywest.com>
2019-03-27 06:39:02 -0600

    added basic logic to game, adjusted blinking and border styles, organized assets folder for sound files

--- index.js
+++ index.js
@@ -1,125 +1,109 @@
+//TODO list:
+//bring in .mp3 sounds and assign to each button
+//display and handle logic for current score and high score
+//display and handle logic for quit button
+
 var buttons = document.getElementsByClassName('button')
-var scoreKeeper 
-var quitButton
 var simonsArray = []
-var playersArray = []
 var counter = 0
-var gameStarted = false
 var whosTurnIsIt = 'computers'
 
-//computers turn:
-// turn 1? pick a color && now players turn 
-//if not {
-//cycle through existing array and blink exisiting pattern
-//beep with each blink
-//after cycle add one more to the array}
-
-//players turn:
-//starting with an empty array duplicate the computers array by clicking colored squares
-//if your next click doesnt match the matching color.id of the (simonsarray) then game over. 
-
-//1st: computers turn
-//2nd: players turn
-//-back to 1st
-//
-//score === simonsArray.length
-//highschore === highest simonsarray.length for this session
-
+// var scoreKeeper
+// var quitButton
 
 //initial setup
 for(let i = 0; i<buttons.length; i++){
     buttons[i].addEventListener('click', handleButton(buttons[i].id))
 }
 
-computersTurn()
+//game logic
+startGame()
 
-function computersTurn (){
-    var seconds = 0
-    if (gameStarted) {
-        cycle(pickNewColor)
-    } else {
-        console.log('Game is Starting! Picking New Color')
-        gameStarted = true
-        pickNewColor(),
-        handlePlayerTurns()
-    }
+function startGame (){
+    console.log('Game is Starting! Picking New Color')
+    setTimeout(()=> {
+        pickNewColor()
+        setTimeout(()=> {
+            handlePlayerTurns()
+        }, 1000)
+    }, 2000)
 }
-
-function cycle (cb) {
-    console.log('[cycle]') 
-    // var beat = setInterval(()=> {
-    //     seconds += 1
-    //     console.log('[beatSeconds:]', seconds)
-    // }, 1000)
-    // setTimeout(()=>{
-    //     clearInterval(beat)
-    //     cbEndTurn()
-    // }, 4000)
-    return function(){
-        simonsArray.forEach( ele => {
-            console.log('executingForEach')
-            setTimeout(()=>{
-                console.log('cycling through ele.s. Current:', ele)
-            },
-            2000)
-            })
-        cb()
-    }
-}
-        
-
 function pickNewColor() {
     var randomButton = buttons[Math.trunc(Math.random()*4)]
     simonsArray.push(randomButton)
-    console.log('executing picknewcolor', simonsArray)
-    //grabs a randomColorTile DOM element 
-    //pushes that randomcolorTile to the simsonArray
+    console.log('[pickNewcolor] simonsArray:', simonsArray)
     blink(randomButton)
 }
+function cycle(cb) {
+    function runCycle(){
+        let i = 0
+        var cycleInterval = setInterval(()=> {
+            blink(simonsArray[i])
+            i++
+            if(i === counter ){
+                counter = 0
 
-function blink(DOMele){
-    //toggle class border ever 100ms
-
-    var blinking = setInterval(()=> {
-        DOMele.classList.toggle('border')
-        }, 100)
-    
-    //after 400 ms stop blinking
-    setTimeout(()=>{
-        clearInterval(blinking)
-        },400)
+                setTimeout(()=>{
+                    clearInterval(cycleInterval) 
+                    cb()
+                    i=0
+                    setTimeout(()=> {
+                        handlePlayerTurns()
+                    }, 500)
+                }, 1000)
+            }
+        }, 1000)
+    }
+    runCycle()
 }
-
 function handleButton(id) {
+    //everything in here happens when our 'click' event triggers
     return function (){
-        console.log('[handleButton], player picked:', simonsArray[counter].id)
-        if (id === simonsArray[counter].id) {
-            playersArray.push(id)
-            counter++
-            handlePlayerTurns()
+        document.getElementById(id).style.background = 'white'
+        setTimeout(()=> {
+            document.getElementById(id).style.background = id
+        }, 80)
+
+        counter++
+        if (counter === simonsArray.length && id === simonsArray[counter-1].id) {
+            handlePlayerTurns() 
+        } else if (id === simonsArray[counter-1].id) {
+            console.log('guess the next one')
         } else {
-            counter = 0
-            console.log('game over')
+            setTimeout(()=> {
+                alert('GAME OVER! \nTry again.')
+                counter = 0
+                simonsArray = []
+                whosTurnIsIt = 'computers'
+                startGame()
+            }, 100)
+
         }
     }
 }
 
-
+function blink(DOMele){
+    var blinking = setInterval(()=> {
+        if(DOMele){
+            DOMele.classList.toggle('border')
+        }
+        }, 100)
+    setTimeout(()=>{
+        if(DOMele){
+            DOMele.classList.remove('border')
+        }
+        clearInterval(blinking)
+        },400)
+}
 
 function handlePlayerTurns(){
     if(whosTurnIsIt === 'computers'){
         whosTurnIsIt = 'player'
-        console.log('[playerTurn]')
-
-    } else {
+        console.log('[playersTurn]')
+        
+    } else if (whosTurnIsIt === 'player' && counter === simonsArray.length){
         whosTurnIsIt = 'computers'
         console.log('[compTurn]')
-        computersTurn()
+        cycle(pickNewColor)
     }
 }
-// after color its players turn
-
-
-
-
-
